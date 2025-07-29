00e23488600bc6d30f19168b5789a80caed0ade9
Ayush Singh <ayusingh693@gmail.com>
2025-07-29 07:11:51 +0000

    comit

--- LLM/1demo.py
+++ LLM/1demo.py
@@ -5,8 +5,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 
 
-model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.7)
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.7)
 
 
-response = model.invoke("Hello, what is the weather like in Surat, Gujarat today?")
+response = model.invoke("capital of india ?")
 print(response.content)
